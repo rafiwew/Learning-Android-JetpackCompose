4353ff4838719b45eac0dc207c5cd5b940fde56f
Muhammad Rafi <mochrafi01@gmail.com>
2023-09-19 18:09:32 +0700

    Stateful, Stateless, State Hoisting practice
    Practice creating a temperature converter application to understand the basics of state in Jetpack Compose

--- Layout/JetCoffee/app/src/main/java/com/piwew/jetcoffee/MainActivity.kt
+++ Layout/JetCoffee/app/src/main/java/com/piwew/jetcoffee/MainActivity.kt
@@ -19,12 +19,16 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -179,4 +183,25 @@ fun JetCoffeeAppPreview() {
     JetCoffeeTheme {
         JetCoffeeApp()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FormInputPreview() {
+    JetCoffeeTheme {
+        FormInput()
+    }
+}
+
+@Composable
+fun FormInput() {
+    var name by rememberSaveable { mutableStateOf("") }
+    OutlinedTextField(
+        value = name,
+        onValueChange = { newName ->
+            name = newName
+        },
+        label = { Text("Nama") },
+        modifier = Modifier.padding(8.dp)
+    )
 }
---
+++ State/MyTemperatureConverter/app/src/main/java/com/piwew/mytemperatureconverter/MainActivity.kt
@@ -0,0 +1,201 @@
+package com.piwew.mytemperatureconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.piwew.mytemperatureconverter.ui.theme.MyTemperatureConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTemperatureConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.verticalScroll(rememberScrollState())
+                    ) {
+                        StatefulTemperatureInput()
+                        ConverterApp()
+                        TwoWayConverterApp()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier
+) {
+    var input by rememberSaveable { mutableStateOf("") }
+    var output by rememberSaveable { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
+
+@Composable
+private fun ConverterApp(
+    modifier: Modifier = Modifier
+) {
+    var input by rememberSaveable {
+        mutableStateOf("")
+    }
+    var output by rememberSaveable {
+        mutableStateOf("")
+    }
+    Column(modifier) {
+        StatelessTemperatureInput(
+            input = input,
+            output = output,
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StatelessTemperatureInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateless_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
+
+enum class Scale(val scaleName: String) {
+    CELSIUS("Celsius"),
+    FAHRENHEIT("Fahrenheit")
+}
+
+@Composable
+private fun TwoWayConverterApp(
+    modifier: Modifier = Modifier
+) {
+    var celsius by rememberSaveable {
+        mutableStateOf("")
+    }
+    var fahrenheit by rememberSaveable {
+        mutableStateOf("")
+    }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.two_way_converter),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        GeneralTemperatureInput(
+            scale = Scale.CELSIUS,
+            input = celsius,
+            onValueChange = { newInput ->
+                celsius = newInput
+                fahrenheit = convertToFahrenheit(newInput)
+            }
+        )
+        GeneralTemperatureInput(
+            scale = Scale.FAHRENHEIT,
+            input = fahrenheit,
+            onValueChange = { newInput ->
+                fahrenheit = newInput
+                celsius = convertToCelsius(newInput)
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GeneralTemperatureInput(
+    scale: Scale,
+    input: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_temperature, scale.scaleName)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+    }
+}
+
+private fun convertToFahrenheit(celsius: String) =
+    celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
+
+private fun convertToCelsius(fahrenheit: String) =
+    fahrenheit.toDoubleOrNull()?.let {
+        (it - 32) * 5 / 9
+    }.toString()
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    MyTemperatureConverterTheme {
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState())
+        ) {
+            StatefulTemperatureInput()
+            ConverterApp()
+            TwoWayConverterApp()
+        }
+    }
+}
