14d9e9dda2c3e7b045a692b8645e00d6757c9dc3
Muhammad Rafi <mochrafi01@gmail.com>
2023-09-30 16:09:56 +0700

    update detail screen

--- Submission/JetApp/app/src/main/java/com/piwew/jetapp/ui/screen/detail/DetailScreen.kt
+++ Submission/JetApp/app/src/main/java/com/piwew/jetapp/ui/screen/detail/DetailScreen.kt
@@ -1,11 +1,34 @@
 package com.piwew.jetapp.ui.screen.detail
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.piwew.jetapp.R
 import com.piwew.jetapp.di.Injection
 import com.piwew.jetapp.helper.ViewModelFactory
 import com.piwew.jetapp.ui.common.UiState
@@ -25,6 +48,7 @@ fun DetailScreen(
             is UiState.Success -> {
                 val data = uiState.data
                 DetailContent(
+                    data.item.photoUrl,
                     data.item.name
                 )
             }
@@ -36,15 +60,61 @@ fun DetailScreen(
 
 @Composable
 fun DetailContent(
+    photoUrl: String,
     name: String,
+    modifier: Modifier = Modifier
 ) {
-    Text(text = name, style = MaterialTheme.typography.bodyLarge)
+    Column(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1f)
+        ) {
+            Box {
+                AsyncImage(
+                    model = photoUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = modifier
+                        .height(400.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { }
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = name,
+                    textAlign = TextAlign.Justify,
+                    style = MaterialTheme.typography.headlineSmall.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                Text(
+                    text = stringResource(R.string.lorem_ipsum),
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Justify
+                )
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DetailContentPreview() {
     JetAppTheme {
-        DetailContent(name = "Muhammad Rafi")
+        DetailContent(name = "Muhammad Rafi", photoUrl = "")
     }
 }
