518a38f4597aa08e122789bd34b5cbe087078602
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-05 22:47:08 +0700

    update about screen

--- Submission/JetApp/app/src/main/java/com/piwew/jetapp/ui/screen/about/AboutScreen.kt
+++ Submission/JetApp/app/src/main/java/com/piwew/jetapp/ui/screen/about/AboutScreen.kt
@@ -1,19 +1,23 @@
 package com.piwew.jetapp.ui.screen.about
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
@@ -43,7 +47,8 @@ fun AboutContent(
     onBackClick: () -> Unit
 ) {
     Column(
-        modifier = modifier.padding(8.dp)
+        modifier = modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box {
             Row(
@@ -60,21 +65,44 @@ fun AboutContent(
                 )
                 Text(
                     text = "About",
-                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp),
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    ),
                     modifier = modifier
                         .padding(start = 8.dp)
                         .weight(1f)
                 )
             }
         }
-        Column {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             AsyncImage(
                 model = stringResource(R.string.profile_picture),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
+                contentDescription = "rafi_photo",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
+                    .padding(8.dp)
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .border(3.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+            Text(
+                text = stringResource(R.string.my_name),
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+            Text(
+                text = stringResource(R.string.my_email),
+                style = TextStyle(
+                    fontSize = 12.sp,
+                )
             )
         }
     }
