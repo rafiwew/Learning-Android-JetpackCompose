0fc629bb20a08229e4f201a766025a6dcda48f91
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-13 13:58:30 +0700

    initial commit

---
+++ Submission/JetProgrammingApp/app/src/main/java/com/piwew/jetprogrammingapp/ui/components/PrimaryButton.kt
@@ -0,0 +1,29 @@
+package com.piwew.jetprogrammingapp.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(52.dp)
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
