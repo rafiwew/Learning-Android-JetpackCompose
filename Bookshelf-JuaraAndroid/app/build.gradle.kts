a8db4f3569920f4ef02665ff816457b0071ab35a
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-30 19:06:23 +0700

    added juaraandroid codelab

--- Bookshelf-JuaraAndroid/app/build.gradle.kts
+++ Bookshelf-JuaraAndroid/app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-compiler:2.44")
     kaptTest("com.google.dagger:hilt-android-compiler:2.44")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0-rc01")
